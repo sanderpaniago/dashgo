edcdeaef32abd1d97a2ae7db15f7a828829d37e6
Sander Paniago <sanderppaniago@gmail.com>
2021-08-01 17:04:33 -0400

    Add axios

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -6,11 +6,11 @@ import { useQuery } from 'react-query'
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
+import { api } from "../../services/api";
 
 export default function UserList() {
-    const { data, isLoading, error } = useQuery('users', async () => {
-        const response = await fetch('http://localhost:3000/api/users')
-        const data = await response.json();
+    const { data, isLoading, isFetching, error } = useQuery('users', async () => {
+        const { data } = await api('/users')
 
         const users = data.users.map(user => ({
             id: user.id,
@@ -40,7 +40,10 @@ export default function UserList() {
 
                 <Box flex='1' borderRadius={8} bg='gray.800' p='8' >
                     <Flex mb='8' justify='space-between' align='center'>
-                        <Heading size='lg' fontWeight='normal'>Usuários</Heading>
+                        <Heading size='lg' fontWeight='normal'>
+                            Usuários
+                            {!isLoading && isFetching && <Spinner size='sm' color='gray.500' ml='4' />}
+                        </Heading>
                         <Link href='/users/create' passHref>
                             <Button
                                 as='a'
