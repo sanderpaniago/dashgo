95a56883bc30b71b67c941044ae7ee3a50f04f7a
Sander Paniago <sanderppaniago@gmail.com>
2021-08-01 10:56:21 -0400

    Config generation data of users faker with faker and miragejs

--- src/services/mirage/index.ts
+++ src/services/mirage/index.ts
@@ -1,28 +1,47 @@
-import { createServer, Model } from 'miragejs'
+import { createServer, Factory, Model } from "miragejs";
+import faker from "faker";
 
 type User = {
     name: string;
     email: string;
-    created_at: string
-}
+    created_at: string;
+};
 
 export function makeServer() {
     const server = createServer({
         models: {
-            user: Model.extend<Partial<User>>({})
+            user: Model.extend<Partial<User>>({}),
+        },
+
+        factories: {
+            user: Factory.extend({
+                name(i: number) {
+                    return faker.internet.userName()
+                },
+                email() { 
+                    return faker.internet.email().toLowerCase()
+                },
+                createAt() { 
+                    return faker.date.recent(10);
+                },
+            }),
+        },
+
+        seeds(server) { 
+            server.createList('user', 200)
         },
 
         routes() {
-            this.namespace = 'api';
+            this.namespace = "api";
             this.timing = 750;
 
-            this.get('/users');
-            this.post('/users');
+            this.get("/users");
+            this.post("/users");
 
-            this.namespace = '';
-            this.passthrough()
-        }
-    })
+            this.namespace = "";
+            this.passthrough();
+        },
+    });
 
     return server;
 }
