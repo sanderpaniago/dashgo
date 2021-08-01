0e203e959aeae62e8a8e433d0d537a1effe37388
Sander Paniago <sanderppaniago@gmail.com>
2021-08-01 11:32:18 -0400

    Listing of user in pages/users

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,6 +1,5 @@
 import { Box, Button, Checkbox, Flex, Heading, Icon, Spinner, Table, Tbody, Td, Text, Th, Thead, Tr, useBreakpointValue } from "@chakra-ui/react";
 import Link from "next/link";
-import { useEffect } from "react";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
 import { useQuery } from 'react-query'
 
@@ -13,7 +12,18 @@ export default function UserList() {
         const response = await fetch('http://localhost:3000/api/users')
         const data = await response.json();
 
-        return data
+        const users = data.users.map(user => ({
+            id: user.id,
+            name: user.name,
+            email: user.email,
+            createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
+                day: '2-digit',
+                month: 'long',
+                year: 'numeric'
+            }),
+        }))
+
+        return users
     })
 
     const isWideVersion = useBreakpointValue({
@@ -66,31 +76,33 @@ export default function UserList() {
                                     </Tr>
                                 </Thead>
                                 <Tbody>
-                                    <Tr>
-                                        <Td px={['4', '4', '6']}>
-                                            <Checkbox colorScheme='pink' />
-                                        </Td>
-                                        <Td>
-                                            <Box>
-                                                <Text fontWeight='bold'>Sander Paniago</Text>
-                                                <Text fontSize='sm' color='gray.300' >sanderppaniago@gmail.com</Text>
-                                            </Box>
-                                        </Td>
-                                        {isWideVersion && (
-                                            <Td>04 de Abril, 2021</Td>
-                                        )}
-                                        <Td>
-                                            <Button
-                                                as='a'
-                                                size='sm'
-                                                fontSize='sm'
-                                                colorScheme='purple'
-                                                leftIcon={<Icon as={RiPencilLine} fontSize='16' />}
-                                            >
-                                                {isWideVersion && 'Editar'}
-                                            </Button>
-                                        </Td>
-                                    </Tr>
+                                    {data.map(user => (
+                                        <Tr key={user.id}>
+                                            <Td px={['4', '4', '6']}>
+                                                <Checkbox colorScheme='pink' />
+                                            </Td>
+                                            <Td>
+                                                <Box>
+                                                    <Text fontWeight='bold'>{user.name}</Text>
+                                                    <Text fontSize='sm' color='gray.300' >{user.email}</Text>
+                                                </Box>
+                                            </Td>
+                                            {isWideVersion && (
+                                                <Td>{user.createdAt}</Td>
+                                            )}
+                                            <Td>
+                                                <Button
+                                                    as='a'
+                                                    size='sm'
+                                                    fontSize='sm'
+                                                    colorScheme='purple'
+                                                    leftIcon={<Icon as={RiPencilLine} fontSize='16' />}
+                                                >
+                                                    {isWideVersion && 'Editar'}
+                                                </Button>
+                                            </Td>
+                                        </Tr>
+                                    ))}
                                 </Tbody>
                             </Table>
 
